415de3839f64d20d0448401f2795e43316e03949
Benfarhat <benfarhat.elyes@gmail.com>
2018-02-14 20:59:34 +0100

    adding some mongoose stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import graphqlHTTP from 'express-graphql';
 
@@ -6,19 +7,32 @@ import schema from './graphql';
 
 const app = express();
 
-mongoose.connect('mongodb://test:pwd@127.0.0.1:27017/starter')
+/*
+$ mongo
+> use starter
+switched to db starter
+> db.createUser({user:"test",pwd:"pwd", roles:[{role:"readWrite",db:"starter"}]})
+*/
+const dbURI = 'mongodb://test:pwd@127.0.0.1:27017/starter';
+mongoose.connect(dbURI, { autoIndex: false })
 
 const db = mongoose.connection;
 
-db.on('error', (err) => {console.log('error on database connection:', err)})
+db.on('error', (err) => {console.log('error on database connection:' + err)})
 .once('open', () => {console.log('Connected to DB')});
 
+process.on('SIGINT', () => {
+  db.close(()=> {
+    console.log('Mongoose disconnected through application termination')
+    process.exit(0)
+  })
+})
 
 app.get('/', (req, res) => {
     res.send();
 })
 
-app.use('/api', graphqlHTTP(() => ({
+app.use('/api', bodyParser.json(), graphqlHTTP(() => ({
     schema,
     graphiql: true,
     pretty: true
